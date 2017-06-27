cea066cf05d78fcd4ab1cba0a6ed442fcbcab2dc
Paszczur <m.paszowski@livechatinc.com>
2017-06-27 13:17:41 +0000

    kinda works

--- src/Form/SettingsForm.php
+++ src/Form/SettingsForm.php
@@ -7,18 +7,20 @@ use Drupal\Core\Form\FormStateInterface;
 use GuzzleHttp\Exception\RequestException;
 use GuzzleHttp\Psr7;
 
+class SettingsForm extends ConfigFormBase
+{
 
-class SettingsForm extends ConfigFormBase {
+	public function getFormId()
+	{
+		return 'livechat_admin_settings_form';
+	}
 
-  public function getFormId() {
-    return 'livechat_admin_settings_form';
-  }
+	protected function getEditableConfigNames()
+	{
+		return ['livechat.settings'];
+	}
 
-  protected function getEditableConfigNames() {
-    return ['livechat.settings'];
-  }
-
-  public function buildForm(array $form, FormStateInterface $form_state)
+	public function buildForm(array $form, FormStateInterface $form_state)
 	{
 		$config = $this->config('livechat.settings');
 
@@ -32,22 +34,17 @@ class SettingsForm extends ConfigFormBase {
 		$form['licence_number'] = [
 			'#type' => 'hidden',
 			'#title' => t('LiveChat licence number:'),
-			'#default_value' => $config->get('licence_number'),
-			'#size' => 30,
-			'#maxlength' => 30,
-			'#required' => TRUE,
-			'#states' => array(
-				'visible' => FALSE
-			)
+			'#value' => $config->get('licence_number'),
+			'#size' => 10,
+			'#maxlength' => 10,
 		];
-
 
 		$form['livechat_login'] = [
 			'#type' => 'textfield',
 			'#title' => t('LiveChat login:'),
 			'#default_value' => $config->get('livechat_login'),
 			'#size' => 30,
-			'#maxlength' => 30,
+			'#maxlength' => 50,
 			'#required' => TRUE,
 			'#states' => array(
 				'visible' => array(
@@ -64,155 +61,86 @@ class SettingsForm extends ConfigFormBase {
 				),
 			)
 		];
-		
-//		$form['buttons'] = array(
-//			'#type' => 'submit',
-//			'#value' => t('Your desired text here'),
-//		);
-//		$form['buttons'] = array(
-//			'#type' => 'save',
-//			'#value' => t('Your desired text here2'),
-//		);
 
 		$form['link_container']['signup_link'] = [
 			'#type' => 'link',
 			'#markup' => '<a href="https://my.livechatinc.com/signup?utm_source=drupal8&utm_medium=integration&utm_campaign=drupal8integration" target="_blank">Click here to create LiveChat 30 day trial</a>'
 		];
-		
-		if($form['licence_number']['#value']!==''){
+
+		if (!empty($form['licence_number']['#value']))
+		{
 			$form['livechat_login']['#attributes'] = array('disabled' => true);
 		}
-		
-		
 
 		return parent::buildForm($form, $form_state);
 	}
-	
-//	function livechat_form_alter(&$form, &$form_state, $form_id) {
-//		if ($form_id == 'livechat-admin-settings-form') { 
-//		  $form['actions']['submit']['#value'] = t('Sign Up');
-//		}
-//	}
 
-	public function secondary_submit_function(array &$form, FormStateInterface $form_state) {
-    
-		$form['livechat_login']['#value'] = "itworked";
+	public function submitForm(array &$form, FormStateInterface $form_state)
+	{
+		if (!$form['livechat_login']['#attributes']['disabled'])
+		{
+			$values = $form_state->getValues();
 
+			$this->config('livechat.settings')
+					->set('licence_number', $values['licence_number'])
+					->set('livechat_login', $values['livechat_login'])
+					->save();
+		}
 		parent::submitForm($form, $form_state);
 	}
-	
-	public function submitForm(array &$form, FormStateInterface $form_state) {
-    		
-	$values = $form_state->getValues();
-	
-    $this->config('livechat.settings')
-      ->set('licence_number', $values['licence_number'])
-	  ->set('livechat_login', $values['livechat_login'])
-      ->save();
 
-    parent::submitForm($form, $form_state);
+	public function validateForm(array &$form, FormStateInterface $form_state)
+	{
+		if ($form['livechat_login']['#attributes']['disabled'])
+		{
+			$this->config('livechat.settings')
+					->set('licence_number', "")
+					->set('livechat_login', "")
+					->save();
+
+			$form['livechat_login']['#default_value'] = "example@example.com";
+		} else if ($form['settings']['#value'] == 1)
+		{
+			$this->getLicense($form, $form_state);
+		} else
+		{
+			$this->createLicence($form, $form_state);
+		}
+
+		parent::validateForm($form, $form_state);
 	}
-  
-  public function validateForm(array &$form, FormStateInterface $form_state)
-	{	
-	  if($form['settings']['#value'] == 1){
-		  $this->getLicense($form, $form_state);
-	  }else {
-		  $this->createLicence($form, $form_state);
-	  }
-	  
-	  parent::validateForm($form, $form_state);
-    }
-  
-  private function createLicence(array &$form, FormStateInterface $form_state) {
-	  if (valid_email_address($form_state->getValue('livechat_login')))
+
+	private function getLicense(array &$form, FormStateInterface $form_state)
+	{
+		if (valid_email_address($form['livechat_login']['#value']))
 		{
 			$client = new \GuzzleHttp\Client();
 			try
 			{
-				$base = "https://www.livechatinc.com/signup/?";
-				$url = "";
-				//name
-				$url .= "name=".urlencode(htmlspecialchars($form['name']['#value'].$form['lastname']['#value']));
-				
-				//email
-				$url .= "&email=".urlencode(htmlspecialchars($form['email']['#value']));
-				//password
-				$url .= "&password=".urlencode(htmlspecialchars($form['password']['#value']));
-				//website
-				$url .= "&website=".urlencode(htmlspecialchars($form['website']['#value']));
-				//timezone
-				$url .= "&timezone_gmt=". drupal_get_user_timezone();
-				//url += '	&action=drupal_signup';
-				$url .= "&action=drupal_signup";
-				//url += '&jsoncallback=?';
-				$url .= "&jsoncallback=?";
-				
-				$res = $client->get($base.$url);
-				
-				
-				$stream = (string) $res->getBody();
-				$stream = str_replace(array( '(', ')' ), '', $stream);
-			
-				if(isset(json_decode($stream)->error)){
-					$form['livechat_login']['#value'] = "please enter valid livechat login";
+				$res = $client->get('https://api.livechatinc.com/license/number/' . $form_state->getValue('livechat_login') . '?json=?', ['http_errors' => false]);
 
-					$form_state->setErrorByName("livechat_login","it is not vaild email");
-					
-				} else {
-					
-					$form_state->setValue('licence_number', json_decode($stream)->response);
-				}
-				
-				
-				
-			} catch (RequestException $e)
-			{
-				return($this->t('Error'));
-			}
-
-		} else {
-			
-			$form['livechat_login']['#value'] = "example@example.com";
-			
-		    $form_state->setErrorByName("livechat_login","this email is invalid");
-		}
-  }
-  private function getLicense(array &$form, FormStateInterface $form_state) {
-	  if (valid_email_address($form_state->getValue('livechat_login')))
-		{
-			$client = new \GuzzleHttp\Client();
-			try
-			{
-				$res = $client->get('https://api.livechatinc.com/license/number/'.$form_state->getValue('livechat_login').'?json=?', ['http_errors' => false]);
-				
 				$stream = Psr7\stream_for($res->getBody());
-				
-				if(isset(json_decode($stream)->error)){
+
+				if (isset(json_decode($stream)->error))
+				{
 					$form['livechat_login']['#value'] = "please enter valid livechat login";
 
-					$form_state->setErrorByName("livechat_login","it is not vaild email");
-					
-				} else {
+					$form_state->setErrorByName("livechat_login", "it is not vaild email");
+				} else
+				{
 					$form_state->setValue('licence_number', json_decode($stream)->number);
-					//$form['livechat_login']['#attributes'] = array('disabled' => true);
 				}
-				
-				
-				
 			} catch (RequestException $e)
 			{
 				return($this->t('Error'));
 			}
+		} else
+		{
 
-		} else {
-			
 			$form['livechat_login']['#value'] = "example@example.com";
-			
-			//drupal_set_message("karramba");
-			
-		    $form_state->setErrorByName("livechat_login","this email is invalid");
+
+			$form_state->setErrorByName("livechat_login", "ten email jest inwalidą");
 		}
-  }
-  
+	}
+
 }
